004125f6de3e50bdd17754d1bf65b7793f624fe1
FuchsFa <fabian.tj75@gmail.com>
2017-05-18 14:00:24 +0200

    Neues HelloWorld hinzugefügt
    Das neue HelloWorld nach dem Tutorial hinzugefügt

---
+++ HelloWorldFF/HelloWorld/HelloWorld/__init__.py
@@ -0,0 +1,17 @@
+'''
+Created on 12.05.2017
+
+@author: FF
+'''
+# coding=utf-8
+from __future__ import absolute_import
+
+import octoprint.plugin
+
+class HelloWorldPlugin(octoprint.plugin.StartupPlugin, 
+                       octoprint.plugin.TemplatePlugin):
+    def on_after_startup(self):
+        self._logger.info("Hello World!")
+        
+__plugin_name__ = "Hello World"
+__plugin_implementation__ = HelloWorldPlugin()
