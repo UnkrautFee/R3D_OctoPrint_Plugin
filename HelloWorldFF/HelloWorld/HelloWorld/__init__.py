4284671e7b79fd61ff9c130546a2e1c2cd550cda
FuchsFa <fabian.tj75@gmail.com>
2017-05-18 14:55:17 +0200

    HelloWorld settings
    Setting für Sprache hinzugefügt

--- HelloWorldFF/HelloWorld/HelloWorld/__init__.py
+++ HelloWorldFF/HelloWorld/HelloWorld/__init__.py
@@ -9,9 +9,16 @@ from __future__ import absolute_import
 import octoprint.plugin
 
 class HelloWorldPlugin(octoprint.plugin.StartupPlugin, 
-                       octoprint.plugin.TemplatePlugin):
+                       octoprint.plugin.TemplatePlugin,
+                       octoprint.plugin.SettingsPlugin):
     def on_after_startup(self):
-        self._logger.info("Hello World!")
+        self._logger.info("Hello World! (more: %s)" % self._settings.get(["url"]))
+        
+    def get_settings_defaults(self):
+        return dict(url="https://en.wikipedia.org/wiki/Hello_world")
+    
+    def get_template_vars(self):
+        return dict(url=self._settings.get(["url"]))
         
 __plugin_name__ = "Hello World"
 __plugin_implementation__ = HelloWorldPlugin()
