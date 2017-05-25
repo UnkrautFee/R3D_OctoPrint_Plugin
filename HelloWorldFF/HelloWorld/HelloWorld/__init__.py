dd84029db4ce7fa740f84a18d2d7350a47deab7f
FuchsFa <fabian.tj75@gmail.com>
2017-05-25 14:58:53 +0200

    Settings-Menü bearbeitet
    Neue jinja2 file hinzugefügt, die eine Option ins Settings-Menü
    hinzufügt

--- HelloWorldFF/HelloWorld/HelloWorld/__init__.py
+++ HelloWorldFF/HelloWorld/HelloWorld/__init__.py
@@ -17,8 +17,11 @@ class HelloWorldPlugin(octoprint.plugin.StartupPlugin,
     def get_settings_defaults(self):
         return dict(url="https://en.wikipedia.org/wiki/Hello_world")
     
-    def get_template_vars(self):
-        return dict(url=self._settings.get(["url"]))
+    def get_template_configs(self):
+        return [
+            dict(type="navbar", custom_bindings=False), 
+            dict(type="settings", custom_bindings=False)
+        ]
         
 __plugin_name__ = "Hello World"
 __plugin_implementation__ = HelloWorldPlugin()
