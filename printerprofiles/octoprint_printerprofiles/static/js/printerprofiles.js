4aca19234cf8381855310fe0bde7817ab5c8b21e
FuchsFa <fabian.tj75@gmail.com>
2017-07-14 11:15:26 +0200

    Sternfarbe reagiert auf click
    Die Farbe der Sterne wird nun auf rot gesetzt, wenn sie angeklickt
    werden.

--- printerprofiles/octoprint_printerprofiles/static/js/printerprofiles.js
+++ printerprofiles/octoprint_printerprofiles/static/js/printerprofiles.js
@@ -50,20 +50,28 @@ $(function() {
         self.setProfileFast = function(profile) {
         OctoPrint.settings.savePluginSettings("printerprofiles", {file_name_fast: profile.displayName}, {});
         console.log("Set fast profile to: " + profile.displayName);
+        $(".icon-star[id$='_fast']").css("color", "#ffff00");
+        $("#" + profile.displayName + "_fast").css("color", "#ff0000");
         return true;
         };
         
         self.setProfileMedium = function(profile) {
         OctoPrint.settings.savePluginSettings("printerprofiles", {file_name_medium: profile.displayName}, {});
         	console.log("Set medium profile to: " + profile.displayName);
+        	$(".icon-star[id$='_medium']").css("color", "#ffff00");
+        	$("#" + profile.displayName + "_medium").css("color", "#ff0000");
         	return true;
         };
         
         self.setProfileSlow = function(profile) {
         OctoPrint.settings.savePluginSettings("printerprofiles", {file_name_slow: profile.displayName}, {});
         	console.log("Set slow profile to: " + profile.displayName);
+        	$(".icon-star[id$='_slow']").css("color", "#ffff00");
+        	$("#" + profile.displayName + "_slow").css("color", "#ff0000");
         	return true;
         };
+        
+        
        
     }
 
